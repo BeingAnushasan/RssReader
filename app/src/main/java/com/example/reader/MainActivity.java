aac0e01f1b879265adf91ff0b002a9e1b94b2e1b
beinganushasan <beinganushasan@gmail.com>
2019-09-20 21:09:28 +0545

    everything added

---
+++ app/src/main/java/com/example/reader/MainActivity.java
@@ -0,0 +1,127 @@
+package com.example.reader;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.reader.Adapter.FeedAdapter;
+import com.example.reader.Common.HTTPDataHandler;
+import com.example.reader.Model.RSSObject;
+import com.google.gson.Gson;
+
+//for connectivity
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.content.Context;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    RSSObject rssObject;
+
+    //RSS link
+    private final String RSS_link="https://code-projects.org/feed";
+    private final String RSS_to_Json_API = "https://api.rss2json.com/v1/api.json?rss_url=";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("Latest Updates");
+        setSupportActionBar(toolbar);
+
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager  = new LinearLayoutManager(getBaseContext(),LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+
+        if(haveNetworkConnection()){
+            loadRSS();
+        }
+
+
+    }
+
+    private void loadRSS() {
+        AsyncTask<String,String,String> loadRSSAsync = new AsyncTask<String, String, String>() {
+
+            ProgressDialog mDialog = new ProgressDialog(MainActivity.this);
+
+            @Override
+            protected void onPreExecute() {
+                mDialog.setMessage("Loading...");
+                mDialog.show();
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                String result;
+                HTTPDataHandler http = new HTTPDataHandler();
+                result = http.GetHTTPData(params[0]);
+                return  result;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+               mDialog.dismiss();
+                rssObject = new Gson().fromJson(s,RSSObject.class);
+                FeedAdapter adapter = new FeedAdapter(rssObject,getBaseContext());
+                recyclerView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+                mDialog.dismiss();
+            }
+        };
+
+        StringBuilder url_get_data = new StringBuilder(RSS_to_Json_API);
+        url_get_data.append(RSS_link);
+        loadRSSAsync.execute(url_get_data.toString());
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.menu_refresh)
+
+            if(haveNetworkConnection()){
+                loadRSS();
+            }
+
+        return true;
+    }
+
+
+    //checking for internet
+    private boolean haveNetworkConnection() {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedWifi || haveConnectedMobile;
+    }
+
+}
